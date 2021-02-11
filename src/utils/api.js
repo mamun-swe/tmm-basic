2cbee7a6c5a56905f939124aec76face4bc82770
Mamun-swe <mhmamun166009@gmail.com>
2021-02-11 18:19:27 +0600

    okk

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,3 @@
 // export const api = "https://jsonplaceholder.typicode.com/"
-export const api = "http://localhost:4000/api/v1/"
+// export const api = "http://localhost:4000/api/v1/"
+export const api = "http://tmmdev.colourbangla.com/api/v1/"
