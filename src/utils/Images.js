642d0e6151945547496059ec290821772cde2665
Mamun-swe <mhmamun166009@gmail.com>
2021-02-28 15:23:11 +0600

    Title & icon update

--- src/utils/Images.js
+++ src/utils/Images.js
@@ -1,7 +1,6 @@
 
 import Logo from '../assets/static/logo.png'
 import Logo2 from '../assets/static/logo2.png'
-import User from '../assets/users/user.jpg'
 import NoData from '../assets/static/no-data.png'
 import Blank from '../assets/static/blank.jpg'
 import fourOfour from '../assets/static/404.png'
@@ -9,7 +8,6 @@ import fourOfour from '../assets/static/404.png'
 export const Images = {
     Logo,
     Logo2,
-    User,
     NoData,
     Blank,
     fourOfour
